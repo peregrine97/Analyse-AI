0f6d27d82cbdc6bceda2432cb5aa285e30418715
Amar <amarssrh@gmail.com>
2026-06-20 01:27:48 +0530

    Update frontend API URL for production

--- react-frontend/src/services/api.js
+++ react-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://analyse-ai.onrender.com",
 });
 
 api.interceptors.request.use(
